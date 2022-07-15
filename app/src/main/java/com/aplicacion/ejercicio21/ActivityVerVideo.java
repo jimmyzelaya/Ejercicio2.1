fb50d89b84416f78626172841b6eba8f64055f4d
Jimmy Zelaya <98270491+jimmyzelaya@users.noreply.github.com>
2022-07-14 23:11:39 -0600

    Initial commit

---
+++ app/src/main/java/com/aplicacion/ejercicio21/ActivityVerVideo.java
@@ -0,0 +1,24 @@
+package com.aplicacion.ejercicio21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class ActivityVerVideo extends AppCompatActivity {
+
+    private VideoView videoView2 ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_video);
+
+        videoView2 =(VideoView)findViewById(R.id.videoView2);
+
+        Uri videoUri = Uri.parse(getIntent().getExtras().getString("videoUri"));
+        videoView2.setVideoURI(videoUri);
+        videoView2.start();
+    }
+}
